274cf56da87f7fdf24631eedee7ced0bbc1ead1a
sangotaro <sango0715@gmail.com>
2014-09-08 00:17:54 +0900

    Update

--- brewfile.sh
+++ brewfile.sh
@@ -52,6 +52,7 @@ brew cask install genymotion || true
 brew cask install iFunBox || true
 brew cask install virtualbox || true
 brew cask install vagrant || true
+brew cask install dropbox || true
 
 # Remove outdated packages
 brew cleanup
